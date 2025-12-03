8eddadd59b5608c21e711462dba07c9278ee7c0b
Deilicis <daviszinkers@gmail.com>
2025-12-03 16:01:52 +0200

    Music, dice

---
+++ Assets/Scripts/DiceAnimatorScript.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class DiceAnimatorScript : MonoBehaviour
+{
+    Animator animator;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void RollDice()
+    {
+        animator.SetBool("isRolling", true);
+    } 
+    public void StopDice()
+    {         
+        animator.SetBool("isRolling", false);
+    }
+}
